a5ce260e3b33e7a1dbfdbe42145f505095aea583
Bipolarr <prostopluggg@gmail.com>
2024-10-15 00:12:46 +0300

    1,2 labs

---
+++ lab1/script.js
@@ -0,0 +1,34 @@
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {}
+    };
+
+for (let i = 0; i < 2; i++) {
+    let movieName;
+    let movieRating;
+
+        while (true) {
+            movieName = prompt('Один из последних просмотренных фильмов?');
+            if (movieName === null || movieName.length > 50 || movieName.trim() === '') {
+                alert('Введите корректное название');
+                continue;
+            }
+            break;
+        }
+
+        while (true) {
+            movieRating = +prompt('На сколько оцените его?');
+            if (movieRating === null) {
+                alert('Введите корректную оценку');
+                continue;
+            }
+            break;
+        }
+         
+        personalMovieDB.movies[movieName] = movieRating;
+        
+    }
+
+console.log(personalMovieDB);
---
+++ lab2/script.js
@@ -0,0 +1,22 @@
+const personalMovieDB = {
+    private: false,
+    movies: {
+        'Пила': 7,
+        'Опенгеймер': 9,
+        '2+2': 8,
+    }
+}
+
+function buildTable(movies) {
+    if (!personalMovieDB.private) {
+        const tableContainer = document.getElementById('table');
+        let table = '<table><tr><th>Название</th><th>Оценка</th></tr>'
+        for (const [title, rating] of Object.entries(movies)) {
+            table += `<tr><td>${title}</td><td>${rating}</td></tr>`;
+        }
+        table += '</table>'; 
+        tableContainer.innerHTML = table;
+    } else return;
+}
+
+buildTable(personalMovieDB.movies); 
