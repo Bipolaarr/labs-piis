fb1089be35c2957e364b3ce639891439ef97e9ef
Bipolarr <prostopluggg@gmail.com>
2024-11-25 18:29:07 +0300

    6

--- lab6/input.js
+++ lab6/input.js
@@ -1,127 +1,122 @@
 const targets = document.querySelectorAll('.target');
-let isDragging = false;
-let isSticky = false;
+
+let activeElement = null;
+let action = null; 
 let offsetX, offsetY;
-let currentElement = null;
 let originalPosition = { top: 0, left: 0 };
 
+
 targets.forEach(target => {
-    // Обработчик мыши для начала перетаскивания
+
+    let newPositionX = div.offsetY;
+    let newPositionY = div.offsetTop;
+
+
     target.addEventListener('mousedown', (event) => {
-        if (isSticky) return; 
 
-        originalPosition.top = target.style.top;
-        originalPosition.left = target.style.left;
-
-        startDragging(target, event.clientX, event.clientY);
-    });
-
-    // Обработчик двойного нажатия
-    target.addEventListener('dblclick', () => {
-        isSticky = true;
-        if (currentElement !== target) {
-            currentElement = target;
-            originalPosition.top = target.style.top || '0px';
-            originalPosition.left = target.style.left || '0px';
+       if (!activeElement) {
+        activeElement = target; 
+        action = 'move';
+        offsetX = event.clientX - div.offsetLeft;
+        offsety = event.clientY - div.offsetTop;
         }
-        target.style.backgroundColor = 'blue'; 
+
     });
 
-    // Обработчик клика для выхода из режима «следующий за пальцем»
-    target.addEventListener('click', () => {
-        if (isSticky && currentElement === target) {
-            isSticky = false;
-            currentElement.style.backgroundColor = 'red';
-            currentElement = null; 
+    target.addEventListener('dblclick', (event) => {
+        if (!activeElement) {
+            activeElement = target;
+            action = 'dblclick';
+            offsetX = event.clientX - div.offsetLeft;
+            offsety = event.clientY - div.offsetTop;
+            target.style.backgroundColor = 'green'
         }
     });
 
-    // Обработчики событий сенсорного экрана
-    target.addEventListener('touchstart', (event) => {
-
-        originalPosition.top = target.style.top;
-        originalPosition.left = target.style.left;
-
-        if (event.touches.length > 1) {
-            resetDragging();
-            return;
-        }
-        const touch = event.touches[0];
-        if (isSticky) {
-            followFinger(touch.clientX, touch.clientY);
-        } else {
-            startDragging(target, touch.clientX, touch.clientY);
-        }
-    });
-
-    target.addEventListener('touchmove', (event) => {
-        if (isDragging || isSticky) {
-            const touch = event.touches[0];
-            followFinger(touch.clientX, touch.clientY);
-        }
-    });
-
-    target.addEventListener('touchend', (event) => {
-        if (isSticky) {
-            return; // Не останавливаем режим
-        }
-        if (isDragging) {
-            stopDragging();
+   
+    target.addEventListener('click', (event) => {
+        if (target == activeElement && action == 'dblClick') {
+            activeElement = null; 
+            action = null; 
+            newPositionX = div.offsetLeft;
+            newPositionY = div.offsetTop;
+            target.style.backgroundColor = 'red';
         }
     });
 });
 
-function startDragging(target, clientX, clientY) {
-    isDragging = true;
-    currentElement = target;
-
-    originalPosition.top = target.style.top || '0px';
-    originalPosition.left = target.style.left || '0px';
-
-    offsetX = clientX - target.getBoundingClientRect().left;
-    offsetY = clientY - target.getBoundingClientRect().top;
-}
-
-function followFinger(clientX, clientY) {
-    if (currentElement) {
-        currentElement.style.left = (clientX - offsetX) + 'px';
-        currentElement.style.top = (clientY - offsetY) + 'px';
-    }
-}
-
-function stopDragging() {
-    isDragging = false;
-    currentElement = null;
-}
-
-function resetDragging() {
-    if (currentElement) {
-        currentElement.style.top = originalPosition.top;
-        currentElement.style.left = originalPosition.left;
-        stopDragging();
-    }
-}
 
 document.addEventListener('mousemove', (event) => {
-    if (isDragging && currentElement) {
-        currentElement.style.left = (event.clientX - offsetX) + 'px';
-        currentElement.style.top = (event.clientY - offsetY) + 'px';
-    } else if (isSticky && currentElement) {
-        currentElement.style.left = (event.clientX - offsetX) + 'px';
-        currentElement.style.top = (event.clientY - offsetY) + 'px';
+    if (target = activeElement && action) {
+        target.style.left = '${event.clientX - offsetX}px'; 
+        target.style.top = '${event.clientY - offsetY}px';
     }
 });
 
-document.addEventListener('mouseup', () => {
-    if (isDragging) {
-        stopDragging();
+
+document.addEventListener('mouseup', (event) => {
+    if (div === activeDiv && action === 'move') {
+        activeDiv = null;
+        action = null;
+        newPositionX = div.offsetLeft;
+        newPositionY = div.offsetTop;
     }
 });
 
 document.addEventListener('keydown', (event) => {
-    if (event.key === 'Escape' && currentElement) {
-        resetDragging();
-        currentElement.style.backgroundColor = 'red'; 
-        currentElement = null;
+    if (event.code == 'Escape' && activeElement == target) {
+        escape(); 
     }
 });
+
+div.addEventListener("touchstart", (e) => {
+    if (!activeElement && action !== 'touch_dblClick') {
+      activeElement = target;
+      action = 'touch_move';
+      offsetX = e.touches[0].clientX - target.offsetLeft;
+      offsetY = e.touches[0].clientY - target.offsetTop;
+      div.style.zIndex = 1000;
+    }
+  });
+
+  document.addEventListener("touchstart", (e) => {
+    if (e.touches.length > 1) escape();
+  });
+
+  document.addEventListener("touchmove", (e) => {
+    if (action) {
+      activeElement.style.left = `${e.touches[0].clientX - offsetX}px`;
+      activeElement.style.top = `${e.touches[0].clientY - offsetY}px`;
+    }
+  });
+
+  const isDblTouch = () => {
+    const now = new Date().getTime();
+    const differenceTime = now - lastTouchTime;
+    lastTouchTime = now;
+    return (differenceTime < 300);
+  }
+
+  const escape = () => {
+    activeElement = null;
+    action = null;
+    activeFinger = false;
+    target.style.left = newPositionX + 'px';
+    target.style.top = newPositionY + 'px';
+    target.style.backgroundColor = 'red';
+  }
+
+  div.addEventListener("touchend", (e) => {
+    if (div === activeDiv && action === 'touch_move') {
+      if (isDblTouch()) {
+        action = 'touch_dblClick';
+        target.style.backgroundColor = 'green';
+        return;
+      }
+      activeElement = null;
+      action = null;
+      newPositionX = target.offsetLeft;
+      newPositionY = target.offsetTop;
+      
+    }
+  });
