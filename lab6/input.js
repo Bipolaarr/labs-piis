d9c019a88b732ebd3b1f741d1d88148355f43ff9
Bipolarr <prostopluggg@gmail.com>
2024-11-25 19:33:46 +0300

    32141

--- lab6/input.js
+++ lab6/input.js
@@ -1,9 +1,10 @@
-const elements = document.querySelectorAll(".target")
+const elements = document.querySelectorAll(".target");
 
 let activeDiv = null;
 let action = null;
 let differenceX, differenceY;
 let lastTouchTime;
+let touchCount = 0; // Счетчик касаний
 
 elements.forEach(div => {
   let newPositionX = div.offsetLeft;
@@ -14,17 +15,16 @@ elements.forEach(div => {
     const differenceTime = now - lastTouchTime;
     lastTouchTime = now;
     return (differenceTime < 300);
-  }
+  };
 
   const escape = () => {
     activeDiv = null;
     action = null;
-    activeFinger = false;
     div.style.left = newPositionX + 'px';
     div.style.top = newPositionY + 'px';
     div.style.backgroundColor = 'red';
     div.style.zIndex = '1';
-  }
+  };
 
   div.addEventListener('mousedown', (e) => {
     if (!activeDiv) {
@@ -32,34 +32,36 @@ elements.forEach(div => {
       action = 'move';
       differenceX = e.clientX - div.offsetLeft;
       differenceY = e.clientY - div.offsetTop;
-      div.style.zIndex = 1000;
     }
-  })
+  });
+
   div.addEventListener('dblclick', (e) => {
     if (!activeDiv) {
       activeDiv = div;
       action = 'dblClick';
       differenceX = e.clientX - div.offsetLeft;
       differenceY = e.clientY - div.offsetTop;
-      div.style.backgroundColor = 'cornflowerblue';
-      div.style.zIndex = 1000;
+      div.style.backgroundColor = 'green';
+
     }
-  })
+  });
+
   document.addEventListener('mousemove', (e) => {
     if (div === activeDiv && action) {
       div.style.left = `${e.clientX - differenceX}px`;
       div.style.top = `${e.clientY - differenceY}px`;
     }
-  })
+  });
+
   div.addEventListener('mouseup', (e) => {
     if (div === activeDiv && action === 'move') {
       activeDiv = null;
       action = null;
       newPositionX = div.offsetLeft;
       newPositionY = div.offsetTop;
-      div.style.zIndex = '1';
     }
-  })
+  });
+
   div.addEventListener('click', (e) => {
     if (div === activeDiv && action === 'dblClick') {
       activeDiv = null;
@@ -67,15 +69,14 @@ elements.forEach(div => {
       newPositionX = div.offsetLeft;
       newPositionY = div.offsetTop;
       div.style.backgroundColor = 'red';
-      div.style.zIndex = '1';
     }
-  })
+  });
+
   document.addEventListener("keydown", (e) => {
     if (e.code === "Escape" && activeDiv === div) {
       escape();
     }
   });
-
 
   div.addEventListener("touchstart", (e) => {
     if (!activeDiv && action !== 'touch_dblClick') {
@@ -83,30 +84,34 @@ elements.forEach(div => {
       action = 'touch_move';
       differenceX = e.touches[0].clientX - div.offsetLeft;
       differenceY = e.touches[0].clientY - div.offsetTop;
-      div.style.zIndex = 1000;
     }
-  })
-  document.addEventListener("touchstart", (e) => {
-    if (e.touches.length > 1) escape();
-  })
+    touchCount++;
+
+    if (touchCount === 3) {
+      escape();
+      touchCount = 0; 
+    }
+  });
+
   document.addEventListener("touchmove", (e) => {
     if (action) {
       activeDiv.style.left = `${e.touches[0].clientX - differenceX}px`;
       activeDiv.style.top = `${e.touches[0].clientY - differenceY}px`;
     }
-  })
+  });
+
   div.addEventListener("touchend", (e) => {
     if (div === activeDiv && action === 'touch_move') {
       if (isDblTouch()) {
         action = 'touch_dblClick';
-        div.style.backgroundColor = 'cornflowerblue';
+        div.style.backgroundColor = 'green';
         return;
       }
       activeDiv = null;
       action = null;
       newPositionX = div.offsetLeft;
       newPositionY = div.offsetTop;
-      div.style.zIndex = '1';
     }
-  })
-})
+    touchCount = 0;
+  });
+});
